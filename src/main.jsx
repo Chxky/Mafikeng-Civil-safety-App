755a01237c728d3b4dbc7b06d1ee049326d398d7
Pardon Mahara <pardon@mahikeng.app>
2026-06-27 17:10:45 +0200

    Enable analytics debug mode

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />
+    <Analytics debug={true} />
   </React.StrictMode>
 );
